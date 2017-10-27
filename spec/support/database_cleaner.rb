7f685f423751895143361056cfdcc497998746b1
dkitchenerable <kitchenerdavid@gmail.com>
2017-10-27 10:44:28 -0400

    get test coverage and test helpers setup correctly

---
+++ spec/support/database_cleaner.rb
@@ -0,0 +1,15 @@
+require 'database_cleaner'
+
+RSpec.configure do |config|
+      config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  # start the transaction strategy as examples are run
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
+end
