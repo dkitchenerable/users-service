9ad97b4d53e818225906d8255d693aa85b40a7d5
dkitchenerable <kitchenerdavid@gmail.com>
2017-10-29 01:34:09 -0400

    finish account service key testing

--- spec/services/account_key_service_spec.rb
+++ spec/services/account_key_service_spec.rb
@@ -1,14 +1,10 @@
 require 'rails_helper'
 
 describe AccountKeyService do
+  let(:user) { FactoryBot.create(:user, email: mock_params[:email]) }
   context "a valid response" do
     it "returns users email and generated account key" do
-       mock_params = {
-           email: "foo@bar.com",
-           account_key: "6dd19673329f532f884120da833e6e5d"
-       }
        VCR.use_cassette 'service/api_response' do
-          user = FactoryBot.create(:user, email: mock_params[:email])
           user.update_attribute(:key, "randomkey")
           service = AccountKeyService.new(user.id)
           response = service.call_api
@@ -18,4 +14,26 @@ describe AccountKeyService do
        end
     end
   end
+
+  context "an invalid response" do
+    it "should raise error" do
+      VCR.use_cassette 'service/api_response' do
+        user.update_attribute(:key, "randomkey")
+        service = AccountKeyService.new(user.id)
+        allow(service).to receive(:valid_response?) { false }
+        expect{service.run}.to raise_error(AccountKeyService::AccountApiUnavailable)
+      end
+    end
+  end
+
+  private
+
+  def mock_params
+    {
+      email: "foo@bar.com",
+      account_key: "6dd19673329f532f884120da833e6e5d"
+    }
+  end
+
 end
+
