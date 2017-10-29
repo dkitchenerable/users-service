2ee2cfb582b1f72110e099cb0f6d3b6fc18aacf2
dkitchenerable <kitchenerdavid@gmail.com>
2017-10-29 14:49:35 -0400

    remove throw pattern from job

--- app/jobs/account_key_job.rb
+++ app/jobs/account_key_job.rb
@@ -1,10 +1,6 @@
 class AccountKeyJob < ActiveJob::Base
   queue_as :default
 
-  rescue_from ::ActiveRecord::RecordNotFound do
-    throw :finished
-  end
-
   rescue_from(AccountKeyService::AccountApiUnavailable) do
     retry_job wait: 5.minutes, queue: :default
   end
